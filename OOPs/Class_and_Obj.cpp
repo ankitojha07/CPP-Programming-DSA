ea17ac1b684f82389e574898bceae72e82cdbb2d
Ankit Ojha <ankitojha1409@gmail.com>
2022-10-03 23:54:53 +0530

    class size

--- OOPs/Class_and_Obj.cpp
+++ OOPs/Class_and_Obj.cpp
@@ -4,12 +4,18 @@ using namespace std;
 class Hero{
 
     // prperties of class
-    int health;
+    int health; // size will be 4
+
+    char a; // size will be 1
+
+    int total; // size will be 4
 
 };
 
 int main(int argc, char const *argv[])
 {
-    
+    Hero h1;
+
+    cout<< "Size of class : "<< sizeof(h1)<<endl;
     return 0;
 }
