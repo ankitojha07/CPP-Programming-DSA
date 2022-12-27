058d13fee582f0f205927d5c19df51df5cafd3c1
Ankit Ojha <ankitojha1409@gmail.com>
2022-12-27 16:19:10 +0530

    selection sort

---
+++ sorting/selection_sort.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min_ind = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min_ind])
+            {
+                min_ind = j;
+            }
+            if (min_ind != j)
+                swap(&arr[j], &arr[min_ind]);
+        }
+    }
+}
+
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main()
+{
+    int arr[] = {7, 6, 9, 34, 5, 67, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    selectionSort(arr, n);
+    printArray(arr, n);
+    return 0;
+}
