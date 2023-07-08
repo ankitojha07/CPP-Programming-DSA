d311cf2125e70d299144e4e66a41da7fe5bfcd4a
Ankit Ojha <ankitojha1409@gmail.com>
2023-07-08 14:49:29 +0530

    1D and 2D vectors in STL

--- CP/start.cpp
+++
@@ -1,7 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main()
-{
-    cout << "Boiler plate test!";
-    return 0;
-}
---
+++ vectors/vectorsSTL.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+
+// Vectors : Dynamic containers (Replacement of Array)
+int main()
+{
+
+    vector<int> arr = {1,2,3,4,5};
+
+    arr.push_back(6);
+    cout << arr.capacity() << " is the capacity of this vector." <<endl;
+    cout << arr.size() << " is the current size of this vestor." <<endl;
+
+    cout<<"Printing all of the elements of the vector : " << endl;
+    for (auto k : arr)
+    {
+        cout << k << " ";
+    }
+    cout<< endl;
+    
+    arr.pop_back();
+    cout << arr.capacity() << " is the capacity of this vector." <<endl;
+    cout << arr.size() << " is the current size of this vestor." <<endl;
+
+    return 0;
+}
