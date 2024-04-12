7c36ef783760d15cca24fc654ab8f4814686812a
Ankit <ankitojha1409@gmail.com>
2024-04-12 23:40:28 +0530

    started cpp

--- OOPs/Class_and_Obj.cpp
+++ OOPs/Class_and_Obj.cpp
@@ -1,7 +1,8 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Hero{
+class Hero
+{
 
     // prperties of class
     int health; // size will be 4
@@ -9,13 +10,12 @@ class Hero{
     char a; // size will be 1
 
     int total; // size will be 4
-
 };
 
 int main(int argc, char const *argv[])
 {
     Hero h1;
 
-    cout<< "Size of class : "<< sizeof(h1)<<endl;
+    cout << "Size of class : " << sizeof(h1) << endl;
     return 0;
 }
