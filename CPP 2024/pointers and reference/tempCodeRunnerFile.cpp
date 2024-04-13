546ac2627c57c0185eb204f9a5a8b218754e0a3d
Ankit <ankitojha1409@gmail.com>
2024-04-13 13:25:37 +0530

    reference var

---
+++ CPP 2024/pointers and reference/tempCodeRunnerFile.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int d = 200;
+    int a = &d;
+
+    cout << "Value of d is : " << d << endl;
+    cout << "Value of &a is : " << &a << endl;
+    cout << "Value of a is : " << a << endl;
+    cout << "Value of &d is : " << &d << endl;
+
+        return 0;
+}
