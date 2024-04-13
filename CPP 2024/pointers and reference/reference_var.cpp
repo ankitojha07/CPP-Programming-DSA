a80d372e52a7edaead09806e86766be84bebc53c
Ankit <ankitojha1409@gmail.com>
2024-04-13 14:07:00 +0530

    reference var extra

--- CPP 2024/pointers and reference/reference_var.cpp
+++ CPP 2024/pointers and reference/reference_var.cpp
@@ -3,8 +3,15 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    int d = 200;
+    int d = 200, c = 100;
     int &a = d;
+
+    a = 500; // it will update the value of d also
+
+    // references cannot be reassigned to refer to another variable once they are initialized. They are essentially aliases for a variable and cannot be rebound.
+
+    // &a = c;
+    // cannot be reassigned
 
     cout << "Value of d is : " << d << endl;
     cout << "Value of &a is : " << &a << endl;
