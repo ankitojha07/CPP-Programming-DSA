d26f5c2999cd888af4e4a57cb93004099a13f69a
Ankit <ankitojha1409@gmail.com>
2024-04-13 16:07:41 +0530

    defining member function out of the class

--- CPP 2024/struct and classes/classes.cpp
+++ CPP 2024/struct and classes/classes.cpp
@@ -16,12 +16,20 @@ public:
     {
         cout << id << " " << name << endl;
     }
+
+    void testFun(); // fun declaration, this will be defined out of the class
 };
+
+void Student::testFun()
+{
+    cout << "Outside definition of function works";
+}
 
 int main()
 {
     Student s;
     // s.id = 23; this can't be done because it si declared as pruivate member in a class
     s.getdata();
+    s.testFun();
     return 0;
 }
