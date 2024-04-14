77e44988025fff884b4b03643ed383d382a33d99
Ankit <ankitojha1409@gmail.com>
2024-04-14 23:44:51 +0530

    constructors

---
+++ CPP 2024/constructor/construct.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Test
+{
+    int a, b;
+
+public:
+    Test(int aa, int bb)
+    {
+        aa = this->a;
+        bb = this->b;
+    }
+
+}
+
+int
+main()
+{
+
+    return 0;
+}
