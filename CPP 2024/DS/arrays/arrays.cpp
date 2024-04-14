7200c96c3183d9d1b6aacf45c741c9128d72b95a
Ankit <ankitojha1409@gmail.com>
2024-04-15 01:27:48 +0530

    arrays in cpp

---
+++ CPP 2024/DS/arrays/arrays.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[] = {1, 8, 9, 3, 6, 4, 78};
+    int n = sizeof(arr) / sizeof(int);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
