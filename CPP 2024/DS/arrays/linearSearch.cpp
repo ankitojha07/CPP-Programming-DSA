61a8fa71b90f25db38b2d69049a80d5a00c2481f
Ankit <ankitojha1409@gmail.com>
2024-04-15 23:50:25 +0530

    linear search storing value of result index

--- CPP 2024/DS/arrays/linearSearch.cpp
+++ CPP 2024/DS/arrays/linearSearch.cpp
@@ -6,16 +6,25 @@ int main()
     int arr[] = {78, 6, 96, 25, 3};
     int n = sizeof(arr) / sizeof(int);
     int num = 3;
+    int res = 0;
     for (int i = 0; i < n; i++)
     {
         if (arr[i] == num)
         {
-            cout << "Element found at " << i << " th index" << endl;
+            res = i;
         }
         else
         {
-            cout << "Element not present in the array" << endl;
+            res = 0;
         }
+    }
+    if (res + 1 == 0)
+    {
+        cout << "Element is not present in the array!" << endl;
+    }
+    else
+    {
+        cout << "Element found at " << res + 1 << " position" << endl;
     }
 
     return 0;
