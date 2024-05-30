962c7d46f5c5a47f0b84451831373ef23ce106df
Ankit <ankitojha1409@gmail.com>
2024-05-31 02:45:00 +0530

    bubble sort done

---
+++ CPP 2024/algorithms/sorting/bubble_sort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {12, 4, 76, 54, 8, 21};
+    int count = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 0; i < count; i++)
+    {
+        int temp = 0;
+        for (int j = 0; j < count; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    cout << endl;
+    for (int i = 0; i < count; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
---
+++ CPP 2024/algorithms/sorting/insertion_sort.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {12, 4, 76, 54, 8, 21};
+    int count = sizeof(arr) / sizeof(arr[0]);
+
+    
+
+    return 0;
+}
