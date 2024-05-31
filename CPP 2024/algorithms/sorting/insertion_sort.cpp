c5ffdfdb79bd13398390429a173ffff05418bc71
Ankit <ankitojha1409@gmail.com>
2024-06-01 02:17:07 +0530

    insertion sort

--- CPP 2024/algorithms/sorting/insertion_sort.cpp
+++ CPP 2024/algorithms/sorting/insertion_sort.cpp
@@ -6,7 +6,10 @@ int main()
     int arr[] = {12, 4, 76, 54, 8, 21};
     int count = sizeof(arr) / sizeof(arr[0]);
 
-    
+    for (int i = 0; i < count; i++)
+    {
+        cout << arr[i] << " ";
+    }
 
     return 0;
 }
