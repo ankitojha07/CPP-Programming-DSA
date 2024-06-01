fceecf49b3538b1a08ea0a009b4d8f041d87b401
Ankit <ankitojha1409@gmail.com>
2024-06-01 18:58:14 +0530

    insertion sort coded

--- CPP 2024/algorithms/sorting/insertion_sort.cpp
+++ CPP 2024/algorithms/sorting/insertion_sort.cpp
@@ -6,6 +6,30 @@ int main()
     int arr[] = {12, 4, 76, 54, 8, 21};
     int count = sizeof(arr) / sizeof(arr[0]);
 
+    for (int i = 1; i < count; i++)
+    {
+        int temp = arr[i], j = i - 1;
+        for (; j >= 0; j--)
+        {
+            if (temp < arr[j])
+            {
+                arr[j + 1] = arr[j];
+                cout << "Shifterd element to right ->" << endl;
+            }
+            else
+            {
+                cout << "No need to shift" << endl;
+                break;
+            }
+        }
+        cout << "1 outer round completed !!! " << endl;
+        cout << "inserting " << temp << " in place of " << arr[j + 1] << endl;
+        arr[j + 1] = temp;
+
+        cout << endl;
+    }
+
+    cout << "Here is your sorted array :" << endl;
     for (int i = 0; i < count; i++)
     {
         cout << arr[i] << " ";
